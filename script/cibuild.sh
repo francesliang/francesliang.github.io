0fbb7d1d8380d36e4bf3e3648fec2533a136b4a9
Xin(Frances) Liang <francesliang@users.noreply.github.com>
2015-09-30 20:45:44 +1000

    Update cibuild.sh

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -10,6 +10,8 @@ cp  -rf _site/* ../francesliang.github.io
 
 # commit and push changes
 cd ../francesliang.github.io
+git config user.email "francisliang2010@hotmail.com"
+git config user.name "Xin(Frances) Liang"
 git add -A .
 git commit -am "Travis #$TRAVIS_BUILD_NUMBER"
 git push
