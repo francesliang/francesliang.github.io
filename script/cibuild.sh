d616c54a0543e60aea37cf82a9bad089797d9c9c
francesliang <francisliang2010@hotmail.com>
2015-10-01 20:29:26 +1000

    Updated cibuild.sh

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -9,13 +9,13 @@ jekyll build
 rm -rf master
 
 # copy generated site to the blog branch
-cp  -R _site/* master
+cp  -R _site/* ../master
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-git clone https://${GH_TOKEN}@github.com/francesliang/francesliang.github.io.git master
+git clone https://${GH_TOKEN}@github.com/francesliang/francesliang.github.io.git ../master
 
 # commit and push changes
-cd master
+cd ../master
 git config user.email "francisliang2010@hotmail.com"
 git config user.name "Xin(Frances) Liang"
 git add -A .
