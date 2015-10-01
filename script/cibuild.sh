bd340d48cc93a249c177713528c7f8e93c605e84
francesliang <francisliang2010@hotmail.com>
2015-10-01 20:23:29 +1000

    updated cibuild.sh

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -5,16 +5,19 @@ set -e # halt script on error
 # build site with jekyll
 jekyll build
 
+# cleanup
+rm -rf master
+
 # copy generated site to the blog branch
-cp  -rf _site/* ../francesliang.github.io.master
+cp  -R _site/* master
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-git clone https://${GH_TOKEN}@github.com/francesliang/francesliang.github.io.git ../francesliang.github.io.master
+git clone https://${GH_TOKEN}@github.com/francesliang/francesliang.github.io.git master
 
 # commit and push changes
-cd ../francesliang.github.io.master
+cd master
 git config user.email "francisliang2010@hotmail.com"
 git config user.name "Xin(Frances) Liang"
 git add -A .
 git commit -am "Travis #$TRAVIS_BUILD_NUMBER"
-git push
+git push --quiet origin master > /dev/null 2>&1
