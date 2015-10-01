f659d4a4d376e27daf8a7d67ea22d7d5a6bfa843
Xin(Frances) Liang <francisliang2010@hotmail.com>
2015-10-01 10:41:09 +0000

    Travis #20

---
+++ cibuild.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+
+# build site with jekyll
+jekyll build
+
+# cleanup
+rm -rf ../master
+
+#clone `master' branch of the repository using encrypted GH_TOKEN for authentification
+git clone https://${GH_TOKEN}@github.com/francesliang/francesliang.github.io.git ../master
+
+# copy generated site to the blog branch
+cp -R _site/* ../master
+
+
+# commit and push changes
+cd ../master
+git config user.email "francisliang2010@hotmail.com"
+git config user.name "Xin(Frances) Liang"
+git add -A .
+git commit -am "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin master > /dev/null 2>&1
